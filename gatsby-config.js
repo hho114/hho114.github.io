6b85acc6d3ebd2143bc6625f0b7216226af53719
hho114 <hho114@csu.fullerton.edu>
2020-08-12 00:18:12 -0700

    update new version with gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,66 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+module.exports = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // Of course you can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
+    siteTitle: "Alex",
+    siteTitleAlt: `Alex - Portfolio`,
+    siteHeadline: "Alex - Gatsby Theme",
+    siteUrl: "https://alexhuyho.com",
+    siteDescription: "Playful and Colorful One-Page portfolio featuring Parallax effects and animations",
+    siteLanguage: "en",
+    siteImage: "/banner.png",
+    author: "@hho114"
+
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Alex Ho`,
+        short_name: `Alex`,
+        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        start_url: `/`,
+        background_color: `#141821`,
+        theme_color: `#f6ad55`,
+        display: `standalone`,
+        icons: [
+          
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-384x384.png`,
+            sizes: `384x384`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+    
+  ].filter(Boolean),
+}
